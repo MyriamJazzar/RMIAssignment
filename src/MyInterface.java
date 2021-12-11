741362312796cf5415def6f15aa6310b4104e226
Myriam Jazzar <82265449+MyriamJazzar@users.noreply.github.com>
2021-12-11 23:00:57 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
+  
+  public String reverse(String ch) throws RemoteException;
+  public char minchar(String ch) throws RemoteException;
+  public String 
 
 }
